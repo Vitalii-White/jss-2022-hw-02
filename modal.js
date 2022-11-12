7b1eb75fcc51fade6eb2c1b9c330c2ffede7fc33
Mr_Vitalii <112243002+Mr-Vitalii@users.noreply.github.com>
2022-11-12 15:52:00 +0200

    Налаштування анімації

---
+++ modal.js
@@ -0,0 +1,28 @@
+
+//*Автоматическая настройка падинга под высоту хедера */ 
+
+/* document.querySelector(".header").getBoundingClientRect().height
+
+const { height: pageHeaderHeight } = document
+    .querySelector(".header")
+    .getBoundingClientRect();
+
+document.body.style.paddingTop = `${pageHeaderHeight}px`; */
+    
+
+
+//* Кнопка "Замовити послугу" в хедерре */
+    (() => {
+        const refs = {
+            openModalBtn: document.querySelector("[data-modal-open]"),
+            closeModalBtn: document.querySelector("[data-modal-close]"),
+            modal: document.querySelector("[data-modal]"),
+        };
+
+        refs.openModalBtn.addEventListener("click", toggleModal);
+        refs.closeModalBtn.addEventListener("click", toggleModal);
+
+        function toggleModal() {
+            refs.modal.classList.toggle("is-hidden");
+        }
+    })();   
