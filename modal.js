0d7153a2b3ebb1a889a6bd31cac7ea0d38cffb74
Mr_Vitalii <112243002+Mr-Vitalii@users.noreply.github.com>
2022-11-27 11:44:46 +0200

    Додавання SASS

--- modal.js
+++ modal.js
@@ -8,21 +8,21 @@ const { height: pageHeaderHeight } = document
     .getBoundingClientRect();
 
 document.body.style.paddingTop = `${pageHeaderHeight}px`; */
-    
+
 
 
 //* Кнопка "Замовити послугу" в хедерре */
-    (() => {
-        const refs = {
-            openModalBtn: document.querySelector("[data-modal-open]"),
-            closeModalBtn: document.querySelector("[data-modal-close]"),
-            modal: document.querySelector("[data-modal]"),
-        };
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
 
-        refs.openModalBtn.addEventListener("click", toggleModal);
-        refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
-        function toggleModal() {
-            refs.modal.classList.toggle("is-hidden");
-        }
-    })();   
+    function toggleModal() {
+        refs.modal.classList.toggle("backdrop--is-hidden");
+    }
+})();   
